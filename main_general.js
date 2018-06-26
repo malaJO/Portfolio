9dd7cf34ddfd7a32a26e88d88920d90cea30c583
malaJO <jovana.pavic@gmail.com>
2018-06-26 14:44:15 +0200

    Adjusted icons in the Contact area, adjusted for mobile (screen size and padding)

--- main_general.js
+++ main_general.js
@@ -22,7 +22,6 @@ playAnim('Slovo_J', 'SlovoJ', 0); //Plays animation is the element is in view, i
 /*Checks how log user has scrolled on Y axis and triggers animation if scrolled enough*/
 function startAnim(){
  var scrollY = window.pageYOffset || document.documentElement.scrollTop;
- console.log(scrollY);
 
   if (scrollY>800 && scrollY<1400) { /*If user scrolled enough to see the top or bottom of the element that is animated, then animation starts*/
 
@@ -71,70 +70,6 @@ spanovi.style.color = blackOpac;
 
 
 //Modals
-
-/*
-
-
-var modals = document.getElementsByClassName('myModall');
-console.log(modals);
-
-for (i=0; i<modals.length; i++) {
-  var btn = document.getElementsByClassName("myBtn")[i];
-  var span = document.getElementsByClassName("close")[i];
-  var modall = modals[i];
-
-  console.log(btn);
-  console.log(span);
-  console.log(modall);
-
-  // When the user clicks on the button, open the modal
-  btn.onclick = function() {
-      modall.style.display = "block";
-  }
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-      modall.style.display = "none";
-  }
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-      if (event.target == modall) {
-          modall.style.display = "none";
-      }
-  }
-
-}*/
-
-//From w3school
-// Get the modal
-/*var modal = document.getElementById('myModal');
-console.log(modal);
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-console.log(btn);
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-console.log(span);
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}*/
 
 //Getting all modal variables
 var btn0 = document.getElementById('modalBtn1');
@@ -194,4 +129,24 @@ btn4.onclick = function() {
 
 endingModall[4].onclick = function() {
   modall[4].style.display = 'none'
+}
+
+
+//Adjusting size of the icons for Contact by width-height ratio
+
+var icon = document.getElementsByClassName('social');
+
+for (i=0; i<icon.length; i++) {
+  if (icon[i].height > icon[i].width) {
+    icon[i].style.height = '64px';
+    icon[i].style.width = 'auto';
+
+  } else if (icon[i].width > icon[i].height){
+    icon[i].style.width = '64px';
+    icon[i].style.height = 'auto';
+
+  } else {
+    icon[i].style.height = '64px';
+    icon[i].style.width = '64px';
+  }
 }
